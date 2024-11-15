d0f0c2ee039489c35a6a890f6f32b18677149d49
AbdullahHamzacebi <abdullah.hmzcebi@gmail.com>
2024-11-15 16:45:11 +0300

    Add project files.

---
+++ MyBlogNight.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlogNight.EntityLayer.Concrete
+{
+    
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+    }
+}
+
+/* 
+  
+ public-private-interval-protected
+
+ public : Herkese açık olan
+ 
+ private : Sadece kendi içinde bulunduğu kısımdan erişim sağlıyor.
+ 
+ interval : Sadece bulunduğu katman içinde erişim sağlayan
+
+ protected : Bulunduğu sınıf ve o sınıfın miras aldığı sınıflardan erişim sağlıyor.
+ 
+ */
---
+++ MyBlogNight.EntityLayer/Concrete/TagCloud.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlogNight.EntityLayer.Concrete
+{
+    // indexleme yani google aramalarında üst sıralara çıkarmaya yarayan şey.
+    // bir etiketin birden çok article'ı bir article'ın da birden çok etiketi olabilir.
+    public class TagCloud 
+    {
+        public int TagCloudId { get; set; }
+        public string Title { get; set; }
+
+    }
+}
