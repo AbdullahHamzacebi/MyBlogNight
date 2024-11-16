efac8c3be88235aa5017b7e0a0692d56a99f3501
AbdullahHamzacebi <abdullah.hmzcebi@gmail.com>
2024-11-16 21:56:09 +0300

    N Katmanlı Mimari

---
+++ MyBlogNight.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,50 @@
+﻿using MyBlogNight.DataAccessLayer.Abstarct;
+using MyBlogNight.DataAccessLayer.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlogNight.DataAccessLayer.Repositories
+{
+    // interface interface i miras alır.
+    // Ama burda bir class interface i miras alıyor.
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        // private : sadece bu sınıfta erişebilir
+        // readonly : sadece okunabilir. 
+        // private readonly : sadece bu sınıftan okunabilir anlamında.
+
+        private readonly BlogContext _context;
+        public void Delete(int id)
+        {
+            var value = _context.Set<T>().Find(id);
+            _context.Set<T>().Remove(value);
+            _context.SaveChanges();
+        }
+
+        public List<T> GetAll()
+        {
+            return _context.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            // altın yeşil çizgisi null gelebilir o yüzden.
+            return _context.Set<T>().Find(id); 
+        }
+
+        public void Insert(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            _context.SaveChanges();
+        }
+    }
+}
