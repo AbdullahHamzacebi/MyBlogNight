6df728d62a314e7016aef8859d7b87ac132916d7
AbdullahHamzacebi <abdullah.hmzcebi@gmail.com>
2024-11-25 17:25:31 +0300

    Component

---
+++ MyBlogNight.PresentationLayer/Controllers/DefaultController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyBlogNight.PresentationLayer.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
+// MVC proje kampı 
+// Udemy .net core adım adım web geliştirme
---
+++ MyBlogNight.PresentationLayer/ViewComponents/_DefaultHeadComponentPartial.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyBlogNight.PresentationLayer.ViewComponents
+{
+    public class _DefaultHeadComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke() 
+        {
+            // Invoke : Çağırmak
+            return View();
+        }
+    }
+}
+/*  
+ 
+    ViewComponent diye klasör açtık onun mantığı Partialview lara bir çeşit altarnatif.
+    Partial url üzerinden ulaşabiliyoruz ama viewcomponentlere ulaşamıyoruz. bu da daha güvenli hale getiriyor. Controllerdaki yükleri de biraz azaltıyor.
+ 
+ */
